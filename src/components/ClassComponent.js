2a471387ed444d8b90738610c98ff48697f2905b
Sowmyam22 <msowmyadeveloper@gmail.com>
2021-07-01 15:21:31 +0530

    react basic app

---
+++ src/components/ClassComponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class ClassComponent extends React.Component {
+  render() {
+    return <p>This is a Class Component!</p>
+  }
+}
+
+export default ClassComponent;
---
+++ src/components/HomeComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ClassComponent from "./ClassComponent";
+
+function HomeComponent() {
+  return (
+    <div>
+      <p>This is a Home Component! (Functional Component)</p>
+      <ClassComponent />
+    </div>
+  )
+}
+
+export default HomeComponent;
