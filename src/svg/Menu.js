f8813ad0442cc7f3c8ca02dcc0fb704ba0538cb5
Sowmyam22 <msowmyadeveloper@gmail.com>
2021-07-13 09:26:17 +0530

    changed the cursor to pointer for the menu icon

--- src/svg/Menu.js
+++ src/svg/Menu.js
@@ -5,7 +5,7 @@ function Menu(props) {
     <div onClick={props.onClick}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        style={{ "width": "20px", "height": "20px" }}
+        style={{ "width": "20px", "height": "20px", "cursor": "pointer" }}
         fill="none"
         viewBox="0 0 24 24"
         stroke="currentColor"
